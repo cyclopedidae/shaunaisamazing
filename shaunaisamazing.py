eaef59408151db583fe95e83385bb2639c72bba9
shauna-hub <62158763+shauna-hub@users.noreply.github.com>
2020-04-30 21:30:20 -0400

    Add files via upload

---
+++ shaunaisamazing.py
@@ -0,0 +1,38 @@
+# Shauna = "amazing"
+
+print("One can only understand ...")
+
+import time
+time.sleep (2)
+print("the true nature of what we call")
+
+time.sleep (2)
+print("Shauna")
+
+time.sleep (2)
+print("if they understand that Shauna is amazing.")
+
+time.sleep (4)
+print("You see . . .")
+
+time.sleep (2)
+print("when we input 'Shauna', we will get the variable: 'amazing'")
+
+time.sleep(2)
+print("Input: Shauna")
+
+time.sleep(4)
+print(". . .")
+
+time.sleep (2)
+print("Calibrating . . .")
+
+time.sleep (4)
+print(". . .")
+
+time.sleep (2)
+print("Calibration complete")
+print("--------------OUTPUT-------------")
+
+time.sleep (3)
+print("amazing")
